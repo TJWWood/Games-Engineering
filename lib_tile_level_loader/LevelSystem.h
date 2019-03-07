752c664fc0a73009b0db9797ed14b5659ef34d77
TJWWood <40295704@live.napier.ac.uk>
2019-03-07 11:46:37 +0000

    Revert "Switch to final supplied engine"
    This reverts commit 948e80e588a2d3aacb729d8602628962902686d6.

--- lib_tile_level_loader/LevelSystem.h
+++ lib_tile_level_loader/LevelSystem.h
@@ -1,69 +1,44 @@
+//levelsystem.h
 #pragma once
 
 #include <SFML/Graphics.hpp>
-#include <maths.h>
 #include <memory>
 #include <string>
 #include <vector>
-
+#include "maths.h"
 #define ls LevelSystem
 
 class LevelSystem {
 public:
-  static void loadLevelFile(const std::string&, float tileSize = 100.0f);
-  static void unload();
-  static void render(sf::RenderWindow& window);
+	enum TILE { EMPTY, START, END, WALL, ENEMY, WAYPOINT };
 
-  typedef unsigned char Tile;
-
-  enum TILES {
-    EMPTY = ' ',
-    START = 's',
-    END = 'e',
-    WALL = 'w',
-    ENEMY = 'n',
-    WAYPOINT = '+'
-  };
-
-  static Tile getTile(sf::Vector2ul);
-
-  static Tile getTileAt(sf::Vector2f);
-
-  static bool isOnGrid(sf::Vector2f);
-
-  static size_t getWidth();
-
-  static size_t getHeight();
-
-  static sf::Vector2f getTilePosition(sf::Vector2ul);
-
-  static std::vector<sf::Vector2ul> findTiles(Tile);
-
-  static sf::Color getColor(Tile t);
-
-  static void setColor(Tile t, sf::Color c);
-
-  static void setOffset(const sf::Vector2f& _offset);
-
-  static const sf::Vector2f& getOffset();
-
-  static float getTileSize();
+	static void loadLevelFile(const std::string&, float tileSize = 100.f);
+	static void render(sf::RenderWindow &window);
+	static sf::Color getColor(TILE t);
+	static void setColor(TILE t, sf::Color c);
+	//Get Tile at grid coordinate
+	static TILE getTile(sf::Vector2ul);
+	//Get Screenspace coordinate of tile
+	static sf::Vector2f getTilePosition(sf::Vector2ul);
+	//get the tile at screenspace pos
+	static TILE getTileAt(sf::Vector2f);
+	static size_t getHeight();
+	static size_t getWidth();
 
 protected:
-  static std::unique_ptr<Tile[]> _tiles;
-  static size_t _width;
-  static size_t _height;
-  static sf::Vector2f _offset;
+	static std::unique_ptr<TILE[]> _tiles; //Internal array of tiles
+	static size_t _width; //how many tiles wide is level
+	static size_t _height; //how many tile high is level
+	static sf::Vector2f _offset; //Screenspace offset of level, when rendered.
+	static float _tileSize; //Screenspace size of each tile, when rendered.
+	static std::map<TILE, sf::Color> _colours; //color to render each tile type
 
-  static std::vector<std::unique_ptr<sf::RectangleShape>> _sprites;
-
-  static void buildSprites(bool optimise = true);
-
-  static float _tileSize; // for rendering
-  static std::map<Tile, sf::Color> _colours;
+	//array of sfml sprites of each tile
+	static std::vector<std::unique_ptr<sf::RectangleShape>> _sprites;
+	//generate the _sprites array
+	static void buildSprites();
 
 private:
-  LevelSystem() = delete;
-
-  ~LevelSystem() = delete;
+	LevelSystem() = delete;
+	~LevelSystem() = delete;
 };
